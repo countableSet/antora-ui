9f3cb2b8a85b4f1e39fda3836e743dc0102a9958
csonguyen <csnguyen@ucdavis.edu>
2022-01-10 14:32:02 -0800

    Add font awesome for icon admonitions (#3)
    1. Instead of manually copying font-awesome.css, added in a script to copy the font-awesome.css file and rewrite the content containing '../fonts/' to map to '~font-awesome/fonts' so the postcss plugin script can find the associated fonts dir and copy the fonts over to our `build/ui-bundle/font` dir
    2. Cleaned up any previous font-awesome.css references in the clean command to always grab the css file pulled in from npm. 
    3. Added a `merge` wrapping the original merge() statement so that the build's ui-bundle didn't contain an extra font-awesome.css file, which was happening because the original merge() was pulling all the vfs.src files and moving them to the ui-bundle.
    
    Co-authored-by: Connie Nguyen <connie.nguyen@workday.com>

--- gulp.d/tasks/build.js
+++ gulp.d/tasks/build.js
@@ -44,7 +44,18 @@ module.exports = (src, dest, preview) => () => {
           const basename = ospath.basename(abspath)
           const destpath = ospath.join(dest, 'font', basename)
           if (!fs.pathExistsSync(destpath)) fs.copySync(abspath, destpath)
-          return path.join('..', 'font', basename)
+          return path.join('..', 'font', basename) + asset.search + asset.hash
+        },
+      },
+      {
+        filter: '**/~font-awesome/fonts/*',
+        url: (asset) => {
+          const relpath = asset.pathname.substr(1)
+          const abspath = require.resolve(relpath)
+          const basename = ospath.basename(abspath)
+          const destpath = ospath.join(dest, 'font', basename)
+          if (!fs.pathExistsSync(destpath)) fs.copySync(abspath, destpath)
+          return path.join('..', 'font', basename) + asset.search + asset.hash
         },
       },
     ]),
@@ -58,73 +69,86 @@ module.exports = (src, dest, preview) => () => {
 
   return merge(
     vfs
-      .src('js/+([0-9])-*.js', { ...opts, sourcemaps })
-      .pipe(uglify())
-      // NOTE concat already uses stat from newest combined file
-      .pipe(concat('js/site.js')),
-    vfs
-      .src('js/vendor/*([^.])?(.bundle).js', { ...opts, read: false })
-      .pipe(
-        // see https://gulpjs.org/recipes/browserify-multiple-destination.html
-        map((file, enc, next) => {
-          if (file.relative.endsWith('.bundle.js')) {
-            const mtimePromises = []
-            const bundlePath = file.path
-            browserify(file.relative, { basedir: src, detectGlobals: false })
-              .plugin('browser-pack-flat/plugin')
-              .on('file', (bundledPath) => {
-                if (bundledPath !== bundlePath) mtimePromises.push(fs.stat(bundledPath).then(({ mtime }) => mtime))
-              })
-              .bundle((bundleError, bundleBuffer) =>
-                Promise.all(mtimePromises).then((mtimes) => {
-                  const newestMtime = mtimes.reduce((max, curr) => (curr > max ? curr : max), file.stat.mtime)
-                  if (newestMtime > file.stat.mtime) file.stat.mtimeMs = +(file.stat.mtime = newestMtime)
-                  if (bundleBuffer !== undefined) file.contents = bundleBuffer
-                  file.path = file.path.slice(0, file.path.length - 10) + '.js'
-                  next(bundleError, file)
-                })
-              )
-          } else {
-            fs.readFile(file.path, 'UTF-8').then((contents) => {
-              file.contents = Buffer.from(contents)
-              next(null, file)
-            })
-          }
+      .src('node_modules/font-awesome/css/font-awesome.css')
+      .pipe(map((file, enc, next) => {
+        fs.readFile(file.path, 'UTF-8').then((contents) => {
+          const result = contents.replace(/\.\.\/fonts/g, '~font-awesome/fonts')
+          fs.writeFile('src/css/font-awesome.css', Buffer.from(result), 'UTF-8', function (error) {
+            if (error) return console.log(error)
+          })
+          next(null, file)
         })
-      )
-      .pipe(buffer())
-      .pipe(uglify()),
-    vfs
-      .src('js/vendor/*.min.js', opts)
-      .pipe(map((file, enc, next) => next(null, Object.assign(file, { extname: '' }, { extname: '.js' })))),
-    // NOTE use this statement to bundle a JavaScript library that cannot be browserified, like jQuery
-    //vfs.src(require.resolve('<package-name-or-require-path>'), opts).pipe(concat('js/vendor/<library-name>.js')),
-    vfs
-      .src(['css/site.css', 'css/vendor/*.css'], { ...opts, sourcemaps })
-      .pipe(postcss((file) => ({ plugins: postcssPlugins, options: { file } }))),
-    vfs.src('font/*.{ttf,woff*(2)}', opts),
-    vfs.src('img/**/*.{gif,ico,jpg,png,svg}', opts).pipe(
-      preview
-        ? through()
-        : imagemin(
-          [
-            imagemin.gifsicle(),
-            imagemin.jpegtran(),
-            imagemin.optipng(),
-            imagemin.svgo({
-              plugins: [
-                { cleanupIDs: { preservePrefixes: ['icon-', 'view-'] } },
-                { removeViewBox: false },
-                { removeDesc: false },
-              ],
-            }),
-          ].reduce((accum, it) => (it ? accum.concat(it) : accum), [])
+      })),
+    merge(
+      vfs
+        .src('js/+([0-9])-*.js', { ...opts, sourcemaps })
+        .pipe(uglify())
+        // NOTE concat already uses stat from newest combined file
+        .pipe(concat('js/site.js')),
+      vfs
+        .src('js/vendor/*([^.])?(.bundle).js', { ...opts, read: false })
+        .pipe(
+          // see https://gulpjs.org/recipes/browserify-multiple-destination.html
+          map((file, enc, next) => {
+            if (file.relative.endsWith('.bundle.js')) {
+              const mtimePromises = []
+              const bundlePath = file.path
+              browserify(file.relative, { basedir: src, detectGlobals: false })
+                .plugin('browser-pack-flat/plugin')
+                .on('file', (bundledPath) => {
+                  if (bundledPath !== bundlePath) mtimePromises.push(fs.stat(bundledPath).then(({ mtime }) => mtime))
+                })
+                .bundle((bundleError, bundleBuffer) =>
+                  Promise.all(mtimePromises).then((mtimes) => {
+                    const newestMtime = mtimes.reduce((max, curr) => (curr > max ? curr : max), file.stat.mtime)
+                    if (newestMtime > file.stat.mtime) file.stat.mtimeMs = +(file.stat.mtime = newestMtime)
+                    if (bundleBuffer !== undefined) file.contents = bundleBuffer
+                    file.path = file.path.slice(0, file.path.length - 10) + '.js'
+                    next(bundleError, file)
+                  })
+                )
+            } else {
+              fs.readFile(file.path, 'UTF-8').then((contents) => {
+                file.contents = Buffer.from(contents)
+                next(null, file)
+              })
+            }
+          })
         )
-    ),
-    vfs.src('helpers/*.js', opts),
-    vfs.src('layouts/*.hbs', opts),
-    vfs.src('partials/*.hbs', opts)
-  ).pipe(vfs.dest(dest, { sourcemaps: sourcemaps && '.' }))
+        .pipe(buffer())
+        .pipe(uglify()),
+      vfs
+        .src('js/vendor/*.min.js', opts)
+        .pipe(map((file, enc, next) => next(null, Object.assign(file, { extname: '' }, { extname: '.js' })))),
+      // NOTE use this statement to bundle a JavaScript library that cannot be browserified, like jQuery
+      //vfs.src(require.resolve('<package-name-or-require-path>'), opts).pipe(concat('js/vendor/<library-name>.js')),
+      vfs
+        .src(['css/site.css', 'css/vendor/*.css'], { ...opts, sourcemaps })
+        .pipe(postcss((file) => ({ plugins: postcssPlugins, options: { file } }))),
+      vfs.src('font/*.{ttf,woff*(2)}', opts),
+      vfs.src('img/**/*.{gif,ico,jpg,png,svg}', opts).pipe(
+        preview
+          ? through()
+          : imagemin(
+            [
+              imagemin.gifsicle(),
+              imagemin.jpegtran(),
+              imagemin.optipng(),
+              imagemin.svgo({
+                plugins: [
+                  { cleanupIDs: { preservePrefixes: ['icon-', 'view-'] } },
+                  { removeViewBox: false },
+                  { removeDesc: false },
+                ],
+              }),
+            ].reduce((accum, it) => (it ? accum.concat(it) : accum), [])
+          )
+      ),
+      vfs.src('helpers/*.js', opts),
+      vfs.src('layouts/*.hbs', opts),
+      vfs.src('partials/*.hbs', opts)
+    ).pipe(vfs.dest(dest, { sourcemaps: sourcemaps && '.' }))
+  )
 }
 
 function postcssPseudoElementFixer (css, result) {
