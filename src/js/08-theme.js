2a8e139997a7b63011075c2ee90a081c44348e4d
ryan.fong <ryan.fong@workday.com>
2022-02-02 15:04:21 -0800

    .

--- src/js/08-theme.js
+++ src/js/08-theme.js
@@ -1,39 +1,20 @@
 /* global localStorage */
 ;(function () {
   'use strict'
-  const stored = localStorage.getItem('theme')
-  console.log('stored = ' + stored)
-  const systemInitiatedDark = window.matchMedia('(prefers-color-scheme: dark)')
-  console.log('system = ' + (systemInitiatedDark.matches ? 'dark' : 'light'))
-  var actual = null
-  if (stored != null) {
-    actual = stored
-    console.log('using stored')
+  let theme = localStorage.getItem('theme')
+  if (theme !== null) {
+    document.documentElement.setAttribute('data-theme', theme)
   } else {
-    actual = systemInitiatedDark.matches ? 'dark' : 'light'
-    console.log('using system')
-  }
-  console.log('actual = ' + actual)
-
-  if (actual === 'dark') {
-    document.documentElement.setAttribute('data-theme', 'dark')
-    console.log('set element dark')
-  } else {
-    document.documentElement.removeAttribute('data-theme')
-    console.log('set element light')
+    const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+    document.documentElement.setAttribute('data-theme',
+      systemDark ? 'dark' : 'light')
   }
 
   const switchButton = document.getElementById('themeSwitch')
   switchButton.addEventListener('click', function (e) {
     e.preventDefault()
-    if (actual === 'dark') {
-      document.documentElement.removeAttribute('data-theme')
-      localStorage.setItem('theme', 'light')
-      console.log('to light')
-    } else {
-      document.documentElement.setAttribute('data-theme', 'dark')
-      localStorage.setItem('theme', 'dark')
-      console.log('to dark')
-    }
+    theme = theme === 'dark' ? 'light' : 'dark'
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
   })
 })()
